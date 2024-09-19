89c5249ec7ee74dde2c65d6941c74e4ee2406243
A-Hesham11 <a.hesham2215@gmail.com>
2024-09-19 14:27:45 +0300

    darkMode

--- src/components/Global/DarkMode/DarkModeToggle.tsx
+++ src/components/Global/DarkMode/DarkModeToggle.tsx
@@ -3,27 +3,24 @@ import { FaMoon } from "react-icons/fa";
 import { MdLightMode } from "react-icons/md";
 
 const DarkModeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const [isDarkMode, setIsDarkMode] = useState<Boolean>(false);
 
   useEffect(() => {
-    const savedMode = localStorage.getItem('darkMode') === 'true';
+    const savedMode = localStorage.getItem("darkMode") === `true`;
     setIsDarkMode(savedMode);
-    document.documentElement.classList.toggle('dark', savedMode);
+    document.documentElement.classList.toggle("dark", savedMode);
   }, []);
 
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    document.documentElement.classList.toggle('dark');
-    localStorage.setItem('darkMode', !isDarkMode);
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("darkMode", String(newMode));
   };
 
   return (
     <button onClick={toggleDarkMode}>
-      {isDarkMode ? (
-        <MdLightMode size={28} />
-      ) : (
-        <FaMoon size={28} />
-      )}
+      {isDarkMode ? <MdLightMode size={28} /> : <FaMoon size={28} />}
     </button>
   );
 };
