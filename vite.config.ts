982a7dc741b0f9530b0057caf9615987cd4a7191
A-Hesham11 <a.hesham2215@gmail.com>
2024-09-19 15:59:38 +0300

    build1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import Checker from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    Checker({ typescript: false })
+  ],
+});
